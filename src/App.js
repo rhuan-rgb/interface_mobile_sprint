f922e0a7a6829bf46fb8ad2a9fc50558983e0200
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-03-24 16:19:31 -0300

    só uma linha 25-03

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen
           name="Login"
           component={() => (
