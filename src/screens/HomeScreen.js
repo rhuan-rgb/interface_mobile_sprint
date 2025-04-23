6d754bf897cab27a316592eee7d75a8f5a16cc75
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-04-23 09:32:30 -0300

    commit de segurança

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,38 @@
 import React from "react";
-import {Text, View, StyleSheet} from "react-native";
-
+import { Text, View, StyleSheet} from "react-native";
 
 export default function HomeScreen() {
-    return (
-        <View>
-            <Text style={styles.teste}>HomeScreen bruh</Text>
-            <Text style={styles.teste}>HomeScreen bruh</Text>
-            <Text style={styles.teste}>HomeScreen bruh</Text>
-            <Text style={styles.teste}>HomeScreen bruh</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <View style={styles.quadrado_cinza}></View>
+      <View style={styles.quadrado_teste}></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    teste:{
-        padding:0,
-        backgroundColor:"purple",
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 50,
+    marginBottom: 70,
+  },
+  teste: {
+    padding: 0,
+    backgroundColor: "purple",
+  },
+  quadrado_cinza: {
+    height: 75,
+    width: 75,
+    backgroundColor: "#D9D9D9",
+    borderRadius: "5px",
+  },
+  quadrado_teste: {
+    height: 75,
+    width: 75,
+    backgroundColor: "yellow",
+    borderRadius: "5px",
+  },
+});
