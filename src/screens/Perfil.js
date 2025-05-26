59545925d3c20d5f14d14e4fc81548e3027e59dd
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-05-26 14:34:06 -0300

    Perfil 50% feito, falta os botões de sair(logout) e trocar senha

--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -1,40 +1,75 @@
-import React, { useEffect } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Touchable,
-  TouchableOpacity,
-} from "react-native";
-// import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, Alert } from "react-native";
 import api from "../axios/axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-async function getCPF(){
-    const cpf = await AsyncStorage.getItem("cpf");
-}
-
-useEffect (()=>{
-    getCPF();
-})
-
-
 export default function Perfil() {
-  let user = api.getUserById(cpf);
-  // const navigation = useNavigation();
+  const [user, setUser] = useState(null);
 
-  user.cpf = user.cpf.split("");
-  for (const i = 0; i === 6; i++) {
-    user.cpf[i] = "*";
-  }
-  user.cpf = user.cpf.join("");
+  useEffect(() => {
+    async function getCPF() {
+      try {
+        const cpf = await AsyncStorage.getItem("cpf");
+        if (cpf) {
+          await api.getUserById(cpf).then(
+            (response) =>{
+              let CPF_escondido = response.data.user.cpf.split("");
+              for (let i = 0; i < 8; i++) {
+                CPF_escondido[i] = "*";
+              }
+              response.data.user.cpf = CPF_escondido.join("");
+              setUser(response.data.user);
+            }, (error) => {
+              Alert.alert("erro ao buscar o usuário");
+            }
+          )
+        }
+      } catch (error) {
+        console.error("Erro ao carregar o perfil:", error);
+      }
+    }
+    getCPF();
+  }, []);
 
   return (
-    <View>
-      <Text style={{ fontSize: 24 }}>Meu perfil</Text>
-      <Text>Nome: {user.name}</Text>
-      <Text>Email: {user.email}</Text>
-      <Text>CPF: {user.cpf}</Text>
+    <View style={styles.container}>
+      {user ? (
+        <>
+          <Text style={styles.header}>Meu perfil</Text>
+          <Text style={styles.info}>Nome: {user.name}</Text>
+          <Text style={styles.info}>Email: {user.email}</Text>
+          <Text style={styles.info}>CPF: {user.cpf}</Text>
+          
+        </>
+      ) : (
+        <Text>Carregando perfil...</Text>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 20,
+    width: '100%',
+  },
+  header: {
+    fontSize: 48,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  info: {
+    fontSize: 36,
+    marginVertical: 5,
+  },
+});
+
+
+
+
+
+
+
