c20e1ae5e2c49bb3694c004283a35a32c450200d
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2025-06-11 11:28:55 -0300

    commit de segurança, falta testar o deletarConta

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,12 +1,11 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios'
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://10.89.240.90:5000/api/reservas/v1/",
-    headers:{
-        'accept':'application/json'
-    }
+  baseURL: "http://10.89.240.90:5000/api/reservas/v1/",
+  headers: {
+    accept: "application/json",
+  },
 });
 
 api.interceptors.request.use(
@@ -21,20 +20,26 @@ api.interceptors.request.use(
 );
 
 const sheets = {
-    
-    postLogin:(user)=>api.post("user/login", user),
-    postCadastro:(user)=>api.post("user", user),
-    getUserById:(cpf)=>api.get(`user/${cpf}`),
-    getAllClassrooms:()=>api.get("classroom"),
-    postSchedule:(sala)=>api.post("schedule", sala),
+  // início
+  postLogin: (user) => api.post("user/login", user),
+  postCadastro: (user) => api.post("user", user),
 
-    getSchedulesByIdClassroomRanges: (class_id, dataInicio, dataTermino ) =>
-        api.get(
-          `/schedule/ranges/${class_id}?weekStart=${dataInicio}&weekEnd=${dataTermino}`
-        ),
-    
-    getScheduleByCpf: (cpf)=> api.get(`schedule/cpf/${cpf}`),
-    deleteSchedule: (schedule)=> api.delete(`schedule/${schedule}`)
-}
+  //perfil
+  getUserById: (cpf) => api.get(`user/${cpf}`),
+  updatePassword: (trocarSenha) => api.put(`/user/newpassword`, trocarSenha),
+  deleteUser: (cpf) => api.delete(`/user/${cpf}`, cpf)
+
+  //salas (reservar as salas)
+  getAllClassrooms: () => api.get("classroom"),
+  postSchedule: (sala) => api.post("schedule", sala),
+  getSchedulesByIdClassroomRanges: (class_id, dataInicio, dataTermino) =>
+    api.get(
+      `/schedule/ranges/${class_id}?weekStart=${dataInicio}&weekEnd=${dataTermino}`
+    ),
+
+  //reservas
+  getScheduleByCpf: (cpf) => api.get(`schedule/cpf/${cpf}`),
+  deleteSchedule: (schedule) => api.delete(`schedule/${schedule}`),
+};
 
 export default sheets;
--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -7,8 +7,13 @@ import ModalBase from "../components/ModalBase";
 
 export default function Perfil() {
   const navigation = useNavigation();
-
+  const [modalDeleteVisible, setModalDeleteVisible] = useState(false);
+  const [modalSenhaVisible, setModalSenhaVisible] = useState(false);
   const [user, setUser] = useState(null);
+  const [trocarSenha, setTrocarSenha] = useState({
+    senha_atual: "",
+    nova_senha: "",
+  });
 
   useEffect(() => {
     async function getCPF() {
@@ -37,17 +42,29 @@ export default function Perfil() {
     getCPF();
   }, []);
 
-  function TrocarSenha(){
+  async function TrocarSenha() {
 
   }
 
+  async function deletarConta(){
+    const cpf = await AsyncStorage.getItem("cpf");
+    await api.deleteUser(cpf).then(
+      ()=>{
+        Alert.alert("conta deletada com sucesso.")
+        Logout();
+      },
+      (error)=>{
+        console.log(error)
+        Alert.alert("erro ao deletar a conta");
+      }
+    )
+  }
 
-  async function Logout(){
+  async function Logout() {
     await AsyncStorage.removeItem("token");
     await AsyncStorage.removeItem("cpf");
     navigation.navigate("Login");
   }
-
 
   return (
     <View style={styles.page}>
@@ -60,10 +77,18 @@ export default function Perfil() {
             <Text style={styles.info}>CPF: {user.cpf}</Text>
           </View>
           <View style={styles.buttons}>
-            <TouchableOpacity onPress={()=>{TrocarSenha()}} style={styles.eachButton}>
+            <TouchableOpacity onPress={() => {}} style={styles.eachButton}>
               <Text style={styles.buttonText}>Trocar senha</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=>{Logout()}} style={styles.eachButton}>
+            <TouchableOpacity onPress={() => {setModalDeleteVisible(true)}} style={styles.eachButton}>
+              <Text style={styles.buttonText}>Deletar conta</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                Logout();
+              }}
+              style={styles.eachButton}
+            >
               <Text style={styles.buttonText}>Sair</Text>
             </TouchableOpacity>
           </View>
@@ -71,6 +96,43 @@ export default function Perfil() {
       ) : (
         <Text>Carregando perfil...</Text>
       )}
+
+      <ModalBase
+        open={modalDeleteVisible}
+        onClose={() => setModalDeleteVisible(!modalDeleteVisible)}
+      >
+        <View>
+  <Text style={{ textAlign: "center", fontSize: 16 }}>
+    Tem certeza de que deseja deletar esta conta?
+  </Text>
+  <View style={styles.modalButtonsContainer}>
+    <TouchableOpacity style={styles.modalButton} onPress={() => deletarConta()}>
+      <Text style={styles.modalButtonText}>SIM</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.modalButton} onPress={() => setModalDeleteVisible(!modalDeleteVisible)}>
+      <Text style={styles.modalButtonText}>NÃO</Text>
+    </TouchableOpacity>
+  </View>
+</View>
+
+      </ModalBase>
+      
+      {/* <ModalBase
+        open={modalSenhaVisible}
+        onClose={setModalSenhaVisible(!prev)}
+      >
+        <View>
+          <Text>Tem certeza de que deseja deletar esta conta?</Text>
+          <View style={styles.}>
+            <TouchableOpacity style={styles.}>
+              <Text>SIM</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.}>
+              <Text>NÃO</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ModalBase> */}
     </View>
   );
 }
@@ -79,11 +141,11 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     padding: 20,
-    justifyContent: "center",   // Centraliza verticalmente
-    alignItems: "center",       // Centraliza horizontalmente
+    justifyContent: "center", // Centraliza verticalmente
+    alignItems: "center", // Centraliza horizontalmente
   },
   container: {
-    alignItems: "center",       // Centraliza textos e itens na view
+    alignItems: "center", // Centraliza textos e itens na view
     marginBottom: 80,
   },
   header: {
@@ -96,7 +158,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginVertical: 5,
     textAlign: "left", // Alinha o texto à esquerda
-    width: 220,        // Define uma largura mínima para que o texto tenha espaço para se alinhar
+    width: 220, // Define uma largura mínima para que o texto tenha espaço para se alinhar
   },
   buttons: {
     alignItems: "center",
@@ -116,4 +178,24 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
   },
+  // modal
+  modalButtonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+  },
+  modalButton: {
+    backgroundColor: "#D90000",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    minWidth: 100,
+    alignItems: "center",
+  },
+  modalButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  
 });
